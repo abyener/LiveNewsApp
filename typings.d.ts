8d75f3c63406a665db3d06e8558157bde5b8e658
ABY <abyener54@gmail.com>
2023-02-04 22:24:02 +0300

    Creeate types & fix home page

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,27 @@
+type Article = {
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type Pagination = {
+  count: Int;
+  limit: Int;
+  ofset: Int;
+  total: Int;
+};
+
+type NewsRespose = {
+  pagination: Pagination;
+  data: Article[];
+};
+
 type Category =
   | "general"
   | "business"
