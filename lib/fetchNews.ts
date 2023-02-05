bdf66bcd09d2aebcbc7716f6537bd7606c524ce2
ABY <abyener54@gmail.com>
2023-02-05 14:46:48 +0300

    Implement graphql & mediastack api

---
+++ lib/fetchNews.ts
@@ -0,0 +1,9 @@
+import { categories } from "../constants";
+
+const fetchNews = async (
+  categories?: Category | String,
+  keywords?: String,
+  isDynamic?: boolean
+) => {};
+
+export default fetchNews;
