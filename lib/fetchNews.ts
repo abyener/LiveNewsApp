b969bd93b926ab1ca8f8303e90fa7870084099e0
ABY <abyener54@gmail.com>
2023-02-16 21:26:02 +0300

    Add stepzen to project

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,9 +1,35 @@
 import { categories } from "../constants";
+import { gql } from "graphql-tag";
 
 const fetchNews = async (
   categories?: Category | String,
   keywords?: String,
   isDynamic?: boolean
-) => {};
+) => {
+  const GET_QUERY = qql`
+  query MyQuery () {
+    myQuery(access_key: "ba7e320de3d5d111ec8ad9af81965d51") {
+      data {
+        author
+        category
+        country
+        description
+        image
+        language
+        published_at
+        source
+        title
+        url
+      }
+      pagination {
+        count
+        limit
+        offset
+        total
+      }
+    }
+  }
+ `;
+};
 
 export default fetchNews;
